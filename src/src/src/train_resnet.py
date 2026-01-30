887a72620c94819cb727c9580751a0a451d37cbc
sandra rose joseph <zandrarosejoseph979@gmail.com>
2026-01-30 22:47:39 +0530

    Create train_resnet.py

---
+++ src/src/src/train_resnet.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import models, transforms, datasets
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+train_data = datasets.ImageFolder("data/processed/train", transform=transform)
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+
+model = models.resnet50(pretrained=True)
+model.fc = nn.Linear(model.fc.in_features, len(train_data.classes))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+for epoch in range(10):
+    for images, labels in train_loader:
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch {epoch+1} completed")
+
+torch.save(model.state_dict(), "models/resnet50.pth")
